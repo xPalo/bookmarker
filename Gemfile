f704a0c3f013d6559c23178afb0f1a634c37d2ee
Adam Palo <xpalo@stuba.sk>
2022-07-20 12:12:47 +0200

    Heroku seed fixes

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ end
 
 group :production do
   gem "rails_12factor", "0.0.2"
+  gem "faker"
 end
 
 group :test do
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,29 +8,40 @@
                       :password_confirmation => 'asdasd'
 end
 
+sleep(0.5)
+
 100.times do
   author = Author.create! :first_name => Faker::Name.first_name,
                         :last_name => Faker::Name.unique.last_name,
                         :country => Faker::Address.country
 end
 
-# 1000.times do
-#   book = Book.create! :title => Faker::Book.unique(max_retries = 1_000_000).title,
-#                       :title_sk => Faker::Book.title,
-#                       :isbn => Faker::Code.unique.isbn,
-#                       :author_id => rand(5..104)
-# end
+sleep(0.5)
 
-# 1000.times do
-#   record = ReadingRecord.create! :user_id => rand(17..68),
-#                       :book_id => rand(16..1054),
-#                       :status => rand(0..2)
-# end
 
-# 1000.times do
-#   record = ReadingRecord.where(status: 2).sample
-#   review = Review.create! :user_id => record.user_id,
-#                           :book_id => record.book_id,
-#                           :score => rand(0..10),
-#                           :body => Faker::Quotes::Shakespeare.hamlet_quote
-# end
+1000.times do
+  book = Book.create! :title => Faker::Book.unique(max_retries = 1_000_000).title,
+                      :title_sk => Faker::Book.title,
+                      :isbn => Faker::Code.unique.isbn,
+                      :author_id => rand(5..104)
+end
+
+sleep(0.5)
+
+
+1000.times do
+  record = ReadingRecord.create! :user_id => rand(17..68),
+                      :book_id => rand(16..1054),
+                      :status => rand(0..2)
+end
+
+sleep(0.5)
+
+
+1000.times do
+  record = ReadingRecord.where(status: 2).sample
+  review = Review.create! :user_id => record.user_id,
+                          :book_id => record.book_id,
+                          :score => rand(0..10),
+                          :body => Faker::Quotes::Shakespeare.hamlet_quote
+end
